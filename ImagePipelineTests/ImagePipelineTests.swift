0d34d085f9441be7fe1dc90ecf94732ffa338824
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2019-01-02 14:24:51 +0900

    Add tests

--- ImagePipelineTests/ImagePipelineTests.swift
+++ ImagePipelineTests/ImagePipelineTests.swift
@@ -208,6 +208,59 @@ class ImagePipelineTests: XCTestCase {
         XCTAssertNil(cache.load(for: url2))
     }
 
+    func testMemoryCacheAutomaticRemovalByCount() {
+        let image0 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image1 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image2 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image3 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image4 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image5 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image6 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image7 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image8 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+        let image9 = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
+
+        let cache = MemoryCache()
+        cache.countLimit = 4
+
+        XCTAssertEqual(cache.countLimit, 4)
+
+        cache.store(image0, for: URL(string: "https://example.com/image\(0)")!)
+        cache.store(image1, for: URL(string: "https://example.com/image\(1)")!)
+        cache.store(image2, for: URL(string: "https://example.com/image\(2)")!)
+        cache.store(image3, for: URL(string: "https://example.com/image\(3)")!)
+        cache.store(image4, for: URL(string: "https://example.com/image\(4)")!)
+        cache.store(image5, for: URL(string: "https://example.com/image\(5)")!)
+        cache.store(image6, for: URL(string: "https://example.com/image\(6)")!)
+        cache.store(image7, for: URL(string: "https://example.com/image\(7)")!)
+        cache.store(image8, for: URL(string: "https://example.com/image\(8)")!)
+        cache.store(image9, for: URL(string: "https://example.com/image\(9)")!)
+
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(0)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(1)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(2)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(3)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(4)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(5)")!))
+        XCTAssertEqual(image6, cache.load(for: URL(string: "https://example.com/image\(6)")!))
+        XCTAssertEqual(image7, cache.load(for: URL(string: "https://example.com/image\(7)")!))
+        XCTAssertEqual(image8, cache.load(for: URL(string: "https://example.com/image\(8)")!))
+        XCTAssertEqual(image9, cache.load(for: URL(string: "https://example.com/image\(9)")!))
+
+        cache.removeAll()
+
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(0)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(1)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(2)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(3)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(4)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(5)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(6)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(7)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(8)")!))
+        XCTAssertNil(cache.load(for: URL(string: "https://example.com/image\(9)")!))
+    }
+
     func testMemoryCacheAutomaticRemoval() {
         let image = UIImage(data: try! Data(contentsOf: Bundle(for: type(of: self)).url(forResource: "200x150", withExtension: "png")!))!
 
@@ -228,6 +281,8 @@ class ImagePipelineTests: XCTestCase {
 
         let cache = MemoryCache()
         cache.totalCostLimit = cost * 4
+
+        XCTAssertEqual(cache.totalCostLimit, cost * 4)
 
         cache.store(image0, for: URL(string: "https://example.com/image\(0)")!)
         cache.store(image1, for: URL(string: "https://example.com/image\(1)")!)
