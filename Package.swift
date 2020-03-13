37998ca6023839e56c58917cc39055c6819c86aa
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2020-03-14 05:51:21 +0900

    Fix webpdecoder version to 1.1.0

--- Package.swift
+++ Package.swift
@@ -6,13 +6,9 @@ import PackageDescription
 let package = Package(
     name: "ImagePipeline",
     platforms: [.iOS(.v10)],
-    products: [
-        .library(
-            name: "ImagePipeline",
-            targets: ["ImagePipeline"]),
-    ],
+    products: [.library(name: "ImagePipeline", targets: ["ImagePipeline"])],
     dependencies: [
-        .package(url: "https://github.com/kishikawakatsumi/webpdecoder.git", .revision("bdba7712fd7e26e6ec3a2e4bcfaaa127b2d59844")),
+        .package(url: "https://github.com/kishikawakatsumi/webpdecoder.git", from: "1.1.0"),
         .package(url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.7.2"),
     ],
     targets: [
